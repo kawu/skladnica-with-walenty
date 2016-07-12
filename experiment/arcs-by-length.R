9d3373ebbab8ef6216da71f888a56494cda6a703
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2016-07-12 19:39:28 +0200

    Add .csv stats files

--- experiment/arcs-by-length.R
+++ experiment/arcs-by-length.R
@@ -4,7 +4,8 @@ data <- read.csv(file=args[1], sep=",")
 
 # plot to window or to file?
 # x11()
-pdf(args[2],width=7,height=4)
+# pdf(args[2],width=7,height=4)
+pdf(args[2])
 
 # margins?
 # par(mar=c(4,4,1,1))
@@ -13,23 +14,25 @@ pdf(args[2],width=7,height=4)
 xs <- data$sent.length
 ys <- data$chart.arcs.1 + data$agenda.arcs.1
 local <- data.frame(length=xs, size=ys)
-aggr <- aggregate(local$size, list(length=local$length), mean)
-
-plot(aggr$length, aggr$x, type="b", col='green'
-	, pch=4
-	# , xlab='(a)\n'
-	, xlab='sentence length'
-	# , xlim=c(4,20)
-	# , ylim=c(1,2000)
-	, ylim=c(1,4500)
-	, ylab='# of arcs of the hypergraph' )
-
+arcs1 <- aggregate(local$size, list(length=local$length), mean)
 
 # number of arcs in 2 step
 ys <- data$chart.arcs.2 + data$agenda.arcs.2
 local <- data.frame(length=xs, size=ys)
-aggr <- aggregate(local$size, list(length=local$length), mean)
-lines(aggr$length, aggr$x, type="b", col='red', pch=1)
+arcs2 <- aggregate(local$size, list(length=local$length), mean)
+
+ymin <- min(arcs1$x, arcs2$x)
+ymax <- max(arcs1$x, arcs2$x)
+
+plot(arcs1$length, arcs1$x, type="b", col='green'
+	, pch=4
+	# , xlab='(a)\n'
+	, xlab='sentence length'
+	, ylim=c(ymin,ymax)
+	, ylab='# of arcs of the hypergraph' )
+
+lines(arcs2$length, arcs2$x, type="b", col='red', pch=1)
+
 
 # add the legend
 legend('bottom',
--- experiment/compare-by-prop.R
+++ experiment/compare-by-prop.R
@@ -3,25 +3,24 @@ args  <- commandArgs(trailingOnly = TRUE)
 dataPath1 <- args[1]
 dataPath2 <- args[2]
 outPath <- args[3]
-print(dataPath1)
-print(dataPath2)
 data1 <- read.csv(file=dataPath1, sep=",")
 data2 <- read.csv(file=dataPath2, sep=",")
 
 # plot to PDF file
-pdf(outPath,width=7,height=4)
+# pdf(outPath,width=7,height=4)
+pdf(outPath)
 
-# stats about arcs for data1
-xs1 <- data1$mwe.deriv.size / data1$reg.deriv.size
-ys1 <- (data1$chart.nodes.1 + data1$agenda.nodes.1) / data1$chart.nodes.2
-local1 <- data.frame(propo=xs1, size=ys1)
-arcs1 <- aggregate(local1$size, list(propo=local1$propo), mean)
+getArcs = function(data) {
+  xs0 <- data$mwe.deriv.size / data$reg.deriv.size
+  xs <- round(xs0,digits=2)
+  ys <- (data$chart.arcs.1 + data$agenda.arcs.1) / data$chart.arcs.2
+  local1 <- data.frame(propo=xs, size=ys)
+  aggregate(local1$size, list(propo=local1$propo), mean)
+}
 
 # stats about arcs
-xs2 <- data2$mwe.deriv.size / data2$reg.deriv.size
-ys2 <- (data2$chart.arcs.1 + data2$agenda.arcs.1) / data2$chart.arcs.2
-local2 <- data.frame(propo=xs2, size=ys2)
-arcs2 <- aggregate(local2$size, list(propo=local2$propo), mean)
+arcs1 <- getArcs(data1)
+arcs2 <- getArcs(data2)
 
 ymin <- min(arcs1$x, arcs2$x)
 ymax <- max(arcs1$x, arcs2$x)
